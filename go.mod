458d0623debdbe8ed16a664ab9495eea424d07ac
VladimirGladky <vovagl23@mail.ru>
2026-02-26 20:20:46 +0300

    fix: add proper golangci-lint module plugin support

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.3
 require golang.org/x/tools v0.42.0
 
 require (
+	github.com/golangci/plugin-module-register v0.1.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
 	golang.org/x/mod v0.33.0 // indirect
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,12 +1,25 @@
-package loglinter
+package linters
 
 import (
 	"github.com/VladimirGladky/SelectelTest"
-
+	"github.com/golangci/plugin-module-register/register"
 	"golang.org/x/tools/go/analysis"
 )
 
-// New is required for golangci-lint module plugins
-func New(conf any) ([]*analysis.Analyzer, error) {
+func init() {
+	register.Plugin("loglinter", New)
+}
+
+type LogLinterPlugin struct{}
+
+func New(settings any) (register.LinterPlugin, error) {
+	return &LogLinterPlugin{}, nil
+}
+
+func (p *LogLinterPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
 	return []*analysis.Analyzer{SelectelTest.Analyzer}, nil
+}
+
+func (p *LogLinterPlugin) GetLoadMode() string {
+	return register.LoadModeSyntax
 }
